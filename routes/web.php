167c176eef49f7d82e8c2b6c8c0e088c8a946a4f
LeoHuang <leohuang1996@gmail.com>
2017-11-28 01:04:12 +0800

    (四)將參數做格式限制

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,14 @@
 //  Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject){
 //       return '學號：'.$student_no.'的'.$subject.'成績';
 //    });
+// Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
+//      return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
+//   });
+
+Route::get('student/{student_no}',function ($student_no){
+  return '學號：'.$student_no;
+  }) -> where(['student_no' => 's[0-9]{10}']);
 
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
-     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
-  });
+  return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
+  }) -> where(['student_no' => 's[0-9]{10}','subject' => '(chinese|english|math)']);
