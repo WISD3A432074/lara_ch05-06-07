825701ad9b65cb7485385712002d3a7d163016c9
LeoHuang <leohuang1996@gmail.com>
2017-11-28 01:27:07 +0800

    (十)新增cool路由

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,4 @@ Route::group(['prefix' => 'student'],function(){
      Route::get('{student_no}',['as' => 'student', 'uses' => 'StudentController@getStudentData']);
      Route::get('{student_no}/score/{subject?}',['as' => 'student.score','uses' => 'StudentController@getStudentScore'])->where(['subject' => '(chinese|english|math)']);
  });
+Route::get('cool', 'Cool\TestController@indexc');
