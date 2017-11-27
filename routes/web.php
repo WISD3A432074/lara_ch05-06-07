5523630e67673defd9ca38286ebb29cc449bdaf1
LeoHuang <leohuang1996@gmail.com>
2017-11-28 01:01:07 +0800

    (三)定義選擇性路由參數

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//
+// Route::get('student/{student_no}',function ($student_no){
+//      return '學號：'.$student_no;
+//  });
+//
+//  Route::get('student/{student_no}/score',function ($student_no){
+//      return '學號：'.$student_no.'的所有成績';
+//  });
+//
+//  Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject){
+//       return '學號：'.$student_no.'的'.$subject.'成績';
+//    });
 
-Route::get('student/{student_no}',function ($student_no){
-     return '學號：'.$student_no;
- });
-
- Route::get('student/{student_no}/score',function ($student_no){
-     return '學號：'.$student_no.'的所有成績';
- });
-
- Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject){
-      return '學號：'.$student_no.'的'.$subject.'成績';
-   });
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
+     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
+  });
